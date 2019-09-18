5decd20de6ae3692cdd6bc922a7ba7b0cbf772d7
B17A2D0B786830A8662BA21E1D748D95 <B17A2D0B786830A8662BA21E1D748D95@i-search.com.cn>
2019-09-18 16:36:28 +0800

    20

--- xc-framework-utils/src/main/java/com/xuecheng/framework/utils/CookieUtil.java
+++ xc-framework-utils/src/main/java/com/xuecheng/framework/utils/CookieUtil.java
@@ -34,7 +34,7 @@ public class CookieUtil {
     /**
      * 根据cookie名称读取cookie
      * @param request
-     * @param cookieName1,cookieName2
+     * @param cookieNames,cookieName1,cookieName2
      * @return map<cookieName,cookieValue>
      */
 
